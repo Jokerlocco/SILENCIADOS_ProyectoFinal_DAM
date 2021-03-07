b92d2cfeb02f6c80d0b56198f1fafa18e4a2d42a
garquesj <jokerlocco@gmail.com>
2021-03-07 17:59:16 +0100

    push 4

--- Assets/Jugador/Scripts/InspeccionDeElementos.cs
+++ Assets/Jugador/Scripts/InspeccionDeElementos.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class InspeccionDeElementos : MonoBehaviour
 {
     [SerializeField] TMP_Text mensajeDeInteraccion; // Asignado en Unity
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerStay(Collider other)
     {
@@ -13,13 +19,21 @@ public class InspeccionDeElementos : MonoBehaviour
             Input.GetButtonDown("Interactuar"))
         {
             if (gameObject.CompareTag("InspeccionPuertaPrincipal"))
-                mensajeDeInteraccion.text = 
+            {
+                mensajeDeInteraccion.text =
                     "La puerta principal está bloqueada. " +
                     "Debo buscar otra forma de salir.";
+                audioSource.Play();
+            }
 
             else if (gameObject.CompareTag("InspeccionAscensor"))
                 mensajeDeInteraccion.text = 
                     "El ascensor está destrozado. No se puede utilizar.";
+
+            else if (gameObject.CompareTag("InspeccionEstatua"))
+                mensajeDeInteraccion.text =
+                    "La estatua tiene tres orificios vacíos. " +
+                    "Es cómo si se tuviera que insertar algo.";
         } 
     }
 
