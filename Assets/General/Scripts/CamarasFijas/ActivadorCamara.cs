32d4011399f677fe8e0e2f2c31bfb6b41a7757f1
garquesj <jokerlocco@gmail.com>
2021-04-03 15:36:02 +0200

    v0.14 - Implementación de un sistema de cámaras fijas (como los Resident Evil clásicos)

---
+++ Assets/General/Scripts/CamarasFijas/ActivadorCamara.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ActivadorCamara : MonoBehaviour
+{
+    private GestorDeCamaras gestorDeCamaras;
+    [SerializeField] private GameObject camaraAActivar;
+
+    private void Start()
+    {
+        gestorDeCamaras = FindObjectOfType<GestorDeCamaras>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Jugador"))
+        {
+            gestorDeCamaras.DesactivarTodasLasCamaras(); // Otra forma de hacer una especie de "SendMessage"
+            camaraAActivar.SetActive(true);
+        }
+    }
+}
---
+++ Assets/General/Scripts/CamarasFijas/GestorDeCamaras.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GestorDeCamaras : MonoBehaviour
+{
+    [SerializeField] private GameObject[] camaras;
+
+    public void DesactivarTodasLasCamaras()
+    {
+        for (int numCamara = 0; numCamara < camaras.Length; numCamara++)
+            camaras[numCamara].SetActive(false);
+    }
+}
